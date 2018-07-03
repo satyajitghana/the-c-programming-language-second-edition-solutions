8fec56afec03df6ef8ae69cf7d68b7ed14d6e22f
Satyajit Ghana <satyajitghana7@gmail.com>
2018-07-04 00:06:45 +0530

    added more solutions

---
+++ Chapter-1/Exercise 1-2/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main() {
+    printf("\"hello world\"\n"); // should print "hello world"
+    //printf("\c") // throws an error since \c is not a valid escape sequence
+}
---
+++ Chapter-1/Exercise 1-7/eof.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// in stdio.h : #define EOF (-1), hence the output is -1
+
+main() {
+    printf("%d\n", EOF);
+}
