c0f24bcb7752f77908df0e8f4d37800ced45c43a
Satyajit Ghana <satyajitghana7@gmail.com>
2018-11-05 23:34:02 +0530

    Add: Exercise 7-1

---
+++ Chapter-7/Exercise 7-1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "print.h"
+
+int main() {
+
+    int a = 2;
+    double b = 3.14159;
+    char* name = "satyajit";
+
+    minprintf("int : %d double : %f string : %s\n", a, b, name);
+    return 0;
+}
